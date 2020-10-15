89873a29a23787e09b92fdec7cdce14c91d0455a
Krista F <55117406+khf0008@users.noreply.github.com>
2020-10-14 20:49:21 -0500

    Add files via upload

---
+++ heyday.py
@@ -0,0 +1,6 @@
+#python version 3.8
+
+#ask the user their name and store it in the 'name' variable
+name = input("Hello Friend! What is your name? ")
+#prints "Hey (name!) by concatenating the strings and variable.
+print ("Hey " + name + "!")
